ec16e554b2e104fa2365262458c98893f42dabeb
bigmachini <elisha.wambiji@bigmachini.net>
2019-03-05 04:11:02 +0300

    interview testing

--- interview_question.py
+++ interview_question.py
@@ -24,3 +24,53 @@ for i in y:
 
 print(''.join(res))
 
+class Node:
+    def __init__(self, data=None):
+        next, data = None, data
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.length = 0
+    def print(self):
+        x = self.head
+        while x  is not None:
+            print(x.data, end=' ')
+            x  = x .next
+            
+
+    def insert(self, positon, data, head):
+        node = Node(data)
+        if positon == 0:
+            tmpNode = head.next
+            head = node
+            node.next = tmpNode
+            self.length += 1
+        elif positon == self.length:
+            self.next = node
+
+        else:
+            i = 0
+            while(head.next):
+                i += 1
+                if i == positon:
+                    node.next = head.next
+                    head.next = node
+
+
+
+        self.length += 1
+
+
+list = LinkedList()
+list.head = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Wed")
+
+# Link first Node to second node
+list.head.nextval = e2
+
+# Link second Node to third node
+e2.nextval = e3
+
+list.print()          
