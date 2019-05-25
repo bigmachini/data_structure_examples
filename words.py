fac827b0b3b53284c918f81b705f5d86b2a19a8b
bigmachini <elisha.wambiji@bigmachini.net>
2019-05-25 18:00:14 +0300

    add arguments to main

--- words.py
+++ words.py
@@ -1,23 +1,27 @@
 from urllib.request import urlopen
+import sys
 
 
-def fetch_words():
-    with urlopen('http://sixty-north.com/c/t.txt')as story:
+def fetch_words(url):
+    with urlopen(url)as story:
         story_words = []
         for line in story:
-            line_words  = line.decode('utf-8').split()
+            line_words = line.decode('utf-8').split()
             for word in line_words:
                 story_words.append(word)
 
         return story_words
 
-def print_words(story_words):
-    for word in story_words:
+
+def print_words(items):
+    for word in items:
         print(word)
 
-def main():
-    words = fetch_words()
+
+def main(url):
+    words = fetch_words(url)
     print_words(words)
 
+
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])
